933f41bd733e8d69838790a3fe7fd7e709680e8a
Josh Kim <realx1212@gmail.com>
2024-12-14 00:00:31 +0900

    a

--- 3-2 override super.dart
+++ 3-2 override super.dart
@@ -1,4 +1,4 @@
-// import 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
 
 String toLocaleString(double number) {
   String formatted = NumberFormat.decimalPattern('ko_KR').format(number);
